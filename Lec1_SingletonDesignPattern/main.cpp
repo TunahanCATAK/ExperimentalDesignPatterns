5887fe6950858c795d078bf9c06e7096cd1bc596
Tunahan Catak <tcatak@Tunahans-MacBook-Pro.local>
2022-07-20 00:01:01 +0200

    Template based singleton object

--- Lec1_SingletonDesignPattern/main.cpp
+++ Lec1_SingletonDesignPattern/main.cpp
@@ -1,58 +1,79 @@
 #include <iostream>
 #include <thread>
 
-class Foo {
+
+// Singleton Class
+template <class T>
+class Singleton {
 
 private:
-    static std::shared_ptr<Foo> m_instance;
+    static std::shared_ptr<T> m_instance;
     std::string m_clientName;
 
 protected:
-    Foo(const std::string& clientName): m_clientName(clientName) {}
+    Singleton(const std::string& clientName): m_clientName(clientName) {}
 
 public:
-    // Singleton object should not be copyable:
-    Foo(Foo& other) = delete;
-    void operator=(const Foo&) = delete;
+    Singleton(Singleton& other) = delete;
+    void operator=(const Singleton&) = delete;
 
-    static std::shared_ptr<Foo> getInstance(const std::string& clientName);
-
-    void TestFunc()
-    {
-        std::cout << "Foo<" << m_clientName << ">.TestFunc() is called." << std::endl;
-    }
+    static std::shared_ptr<T> getInstance(const std::string& clientName);
 };
 
-std::shared_ptr<Foo> Foo::m_instance = nullptr;
-std::shared_ptr<Foo> Foo::getInstance(const std::string& clientName)
+template<typename T>
+std::shared_ptr<T> Singleton<T>::m_instance = nullptr;
+template<typename T>
+std::shared_ptr<T> Singleton<T>::getInstance(const std::string &clientName)
 {
     if (m_instance == nullptr)
     {
-        m_instance = std::shared_ptr<Foo>( new Foo(clientName));
+        m_instance = std::shared_ptr<T>( new T(clientName));
     }
 
     return m_instance;
+}
+
+// User Defined Classes: 
+class Bar {
+private:
+    std::string m_clientName;
+public:
+    Bar(const std::string& clientName): m_clientName(clientName) {}
+    void TestFunc()
+    {
+        std::cout << "Bar<" << m_clientName << ">.TestFunc() is called." << std::endl;
+    }
+};
+
+class NoPublicConstructor{
+private:
+    std::string m_clientName;
+    NoPublicConstructor(const std::string& clientName): m_clientName(clientName) {}
+public:
+    void TestFunc()
+    {
+        std::cout << "NoPublicConstructor<" << m_clientName << ">.TestFunc() is called." << std::endl;
+    }
 };
 
 int main() {
     std::cout << "Singleton Design Pattern Client Usage" << std::endl;
 
-    //auto FooObj = Foo::getInstance("ClientA");
-    //FooObj->TestFunc();
+    auto getSingletonBar = [](const std::string& clientName) -> std::shared_ptr<Bar> {
+        auto BarObj = Singleton<Bar>::getInstance(clientName);
+        BarObj->TestFunc();
 
-    auto getSingleton = [](const std::string& clientName) -> std::shared_ptr<Foo> {
-        auto FooObj = Foo::getInstance(clientName);
-        FooObj->TestFunc();
-
-        return FooObj;
+        return BarObj;
     };
 
-    std::thread thread1(getSingleton, "thread1");
-    std::thread thread2(getSingleton, "thread2");
+    std::thread thread3(getSingletonBar, "thread3");
+    std::thread thread4(getSingletonBar, "thread4");
 
-    thread1.join();
-    thread2.join();
+    thread3.join();
+    thread4.join();
 
+//    auto errorObj = Singleton<NoPublicConstructor>::getInstance("ClientA"); -> Compiler Error, calling a private constructor.
+//    errorObj->TestFunc();
 
     return 0;
 }
